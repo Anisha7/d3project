65e19b70392d7742212a16634288a478ce7292e7
Anisha Jain <anishajain@outlook.com>
2020-02-27 22:20:09 -0800

    songs added and formatted

--- parser.js
+++ parser.js
@@ -1,5 +1,5 @@
-
-const getTopSongs = (csv, artist) => {       
+const numSections = 4
+const getTopSongs = (csv, artist, total) => {       
     // Find all songs and their popularities in given genre
     const songs = {}
     csv.filter((element) => element["Artist"] === artist).forEach(element => {
@@ -18,7 +18,10 @@ const getTopSongs = (csv, artist) => {
     })
 
     // Find top 3 songs
-    const topSongs = songKeys.splice(0, 3)
+    console.log(total)
+    const n = total*numSections*0.03 // random math rn...
+    console.log(n)
+    const topSongs = songKeys.splice(0, n)
 
     const data = []
     topSongs.forEach(s => {
@@ -33,15 +36,18 @@ const getTopSongs = (csv, artist) => {
 
 }
 
-const getTopArtists = (csv, genre) => {
+const getTopArtists = (csv, genre, total) => {
     // Find all artists and their popularities in given genre
     const artists = {}
+    const counts = {}
     csv.filter((element) => element["Top Genre"] === genre).forEach(element => {
         const a = element["Artist"].slice(0, 18)
         if (a in artists) {
             artists[a] += parseInt(element["Popularity"])
+            counts[a] += 1
         } else {
             artists[a] = parseInt(element["Popularity"])
+            counts[a] = 1
         }
     })
     // Sort the artists by their popularity
@@ -49,16 +55,18 @@ const getTopArtists = (csv, genre) => {
     artistKeys.sort(function (a, b) {
         return artists[b] - artists[a];
     })
-
+    console.log(total)
+    const n = total*numSections*0.03 // random math rn...
+    console.log(n)
     // Find top 3 artists
-    const topArtists = artistKeys.splice(0, 3)
+    const topArtists = artistKeys.splice(0, n) // n  instead of numSections
 
     const data = []
     topArtists.forEach(a => {
         data.push(
             {
                 name: a,
-                children: getTopSongs(csv, a)
+                children: getTopSongs(csv, a, artists[a]/counts[a])
             }
         )
     })
@@ -69,13 +77,16 @@ const getTopArtists = (csv, genre) => {
 const getTopGenres = (csv) => {
     // Parse genres and their popularities in a dictionary
     const genres = {} // name -> popularity
+    const counts = {}
     csv.forEach(element => {
         const g = element["Top Genre"]
         if (g in genres) {
             genres[g] += parseInt(element["Popularity"])
+            counts[g] += 1
 
         } else {
             genres[g] = parseInt(element["Popularity"])
+            counts[g] = 1
         }
     });
 
@@ -85,14 +96,14 @@ const getTopGenres = (csv) => {
         return genres[b] - genres[a];
     })
     // Find top 3 genres
-    const topGenres = genreKeys.splice(0, 3) // ["genre", ...]
+    const topGenres = genreKeys.splice(0, numSections) // ["genre", ...]
 
     const data = []
     topGenres.forEach(g => {
         data.push(
             {
                 name: g,
-                children: getTopArtists(csv, g)
+                children: getTopArtists(csv, g, genres[g]/counts[g]) // last parameter should be popularity percentage
             }
         )
     })
